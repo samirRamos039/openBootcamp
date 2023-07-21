ed0a886b0e1a68367a16f1fc81d206f43cde185e
rzagza039@gmail.com <rzagza039@gmail.com>
2023-07-20 19:55:34 -0500

    second commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+       int num = 4;
+       long num2 = 104555566;
+       double num3 = 111111011.22;
+       boolean ToF = false;
+       String nombre = "samir";
+
+       System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(ToF);
+        System.out.println(nombre);
+        }
+    }
