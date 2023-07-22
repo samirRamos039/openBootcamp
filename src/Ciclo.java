6e448d7ec40045cf3a6c3d3c2277dfa4b100a1e2
rzagza039@gmail.com <rzagza039@gmail.com>
2023-07-22 10:47:32 -0500

    Ciclos

---
+++ src/Ciclo.java
@@ -0,0 +1,11 @@
+public class Ciclo {
+
+    public static void main(String[] arg) {
+
+        String[] nombres = {"Samir", "esta", "realizando", "el bootCamp","java basico"};
+
+        for (int i = 0; i< nombres.length; i++){
+            System.out.print(nombres[i]+" ");
+        }
+    }
+}
